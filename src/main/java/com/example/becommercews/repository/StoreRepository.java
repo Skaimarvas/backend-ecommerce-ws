5927f8c4c89274117c402b7abe1181f361ecde7e
skaimarvas <skaimarvas@gmail.com>
2024-03-03 21:00:51 +0300

    StoreRepository was created

---
+++ src/main/java/com/example/becommercews/repository/StoreRepository.java
@@ -0,0 +1,7 @@
+package com.example.becommercews.repository;
+
+import com.example.becommercews.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store,Long> {
+}
