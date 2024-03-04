3c6f824955e358b248478913e8459ab26fe24e1a
skaimarvas <skaimarvas@gmail.com>
2024-03-04 15:07:10 +0300

    findUserByEmail query was added on UserRepository

--- src/main/java/com/example/becommercews/repository/UserRepository.java
+++ src/main/java/com/example/becommercews/repository/UserRepository.java
@@ -2,6 +2,11 @@ package com.example.becommercews.repository;
 
 import com.example.becommercews.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findUserByEmail(String email);
 }
